f0b7f55aa63e99fd1e331c6c9eab489ae2acf71d
Ankita161198 <shinde.ankita1603@gmail.com>
2024-02-21 19:30:44 -0500

    Additional Pages and functionalities

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (12)</div>
+
+        </div>
+        <div className="descriptionbox-description">
+            <p>An ecommerce website is an online platform where businesses sell 
+                products or services to customers over the 
+                internet. These websites are designed to
+                 facilitate transactions between buyers 
+                 and sellers, allowing customers to browse 
+                 products, make purchases, and complete transactions entirely online</p>
+     <p>An ecommerce website is an online platform where businesses sell products or services to customers over the internet.
+       These websites are designed to facilitate transactions between buyers and sellers, allowing customers to browse products, make purchases, and complete transactions entirely online</p>
+        </div>
+    </div>
+  )
+}
